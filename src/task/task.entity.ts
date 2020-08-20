ddbdf9f35a955fd4f654f2f567809f4fa9363d42
Alimustoofaa <softhouse.ali@gmail.com>
2020-08-20 17:04:24 +0700

    add task repository

---
+++ src/task/task.entity.ts
@@ -0,0 +1,10 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { TaskStatus } from "./task.model";
+
+@Entity()
+export class Task extends BaseEntity {
+    @PrimaryGeneratedColumn() id: number;
+    @Column() tittle: string;
+    @Column() description: string;
+    @Column() status: TaskStatus;
+}

9164f81be290a825350ecb1146ee4e4872074025
Alimustoofaa <softhouse.ali@gmail.com>
2020-08-24 00:19:02 +0700

    sign jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,29 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtSrategy } from './jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: 'TopSecret52',
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    JwtSrategy,
+  ],
+  exports: [
+    JwtSrategy,
+    PassportModule,
+  ],
 })
 export class AuthModule {}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -9,7 +9,6 @@ export class UserRepository extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
-        
         const user = new User();
         user.username = username;
         user.salt = await bcrypt.genSalt();
@@ -23,6 +22,17 @@ export class UserRepository extends Repository<User> {
             } else {
                 throw new InternalServerErrorException();
             }
+        }
+    }
+
+    async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.findOne({ username });
+
+        if (user && await user.validatePassword(password)) {
+            return user.username;
+        } else {
+            return null;
         }
     }
 
